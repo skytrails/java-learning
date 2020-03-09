03f9af8ac6e7d2423a4f78f4188e0fdb8ebdd123
skytrails <skytrails@163.com>
2020-03-10 00:40:35 +0800

    打包jar程序的docker镜像包，并上传到docker服务

---
+++ java-docker/src/com/java/docker/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.java.docker;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World, I want to publish to docker.");
+    }
+}
