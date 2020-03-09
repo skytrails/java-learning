e49494c9a90934337d54cc1c3a49d3b8aa86b00a
skytrails <skytrails@163.com>
2020-03-09 22:59:15 +0800

    java helloworld

---
+++ java-helloworld/src/com/java/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.java.helloworld;
+
+/**
+ * @Author: skytrails
+ * @Date: 2020/03/09 下午10:48
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!!!");
+    }
+}
